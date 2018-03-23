da32384e68a7f14331829235f4c8fbe1a7cf5a14
WDirk1 <37687601+WDirk1@users.noreply.github.com>
2018-03-23 09:23:49 +0100

    Second try

---
+++ Vanna/Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour {
+
+	public bool checkpointActive;
+
+
+	// Use this for initialization
+	void Start () {
+		
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.tag == "Player") 
+		{
+			checkpointActive = true;
+		}
+	}
+}
